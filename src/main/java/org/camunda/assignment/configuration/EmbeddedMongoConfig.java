9c428c9bc2d1953d81fa39c9c07625b2ea840813
Ashish Devi <ashishdevi@gmail.com>
2024-05-30 20:24:30 +0200

    DOckerized the application, added env property for testing purpose, modified the api specifications, commented MongoDB jar

--- src/main/java/org/camunda/assignment/configuration/EmbeddedMongoConfig.java
+++ src/main/java/org/camunda/assignment/configuration/EmbeddedMongoConfig.java
@@ -1,32 +1,58 @@
-package org.camunda.assignment.configuration;
-
-import com.mongodb.client.MongoClient;
-import de.flapdoodle.embed.mongo.MongodExecutable;
-import de.flapdoodle.embed.mongo.MongodProcess;
-import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.MongodConfig;
-import de.flapdoodle.embed.mongo.distribution.Version;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoTemplate;
-
-
-import java.io.IOException;
-
-@Configuration
-public class EmbeddedMongoConfig {
-
-//    private static final String MONGO_DB_URL = "localhost";
-//    private static final String MONGO_DB_NAME = "embeded_db";
-//    @Bean
-//    public MongoTemplate mongoTemplate() throws IOException {
-//        EmbeddedMongoFactoryBean mongo = new EmbeddedMongoFactoryBean();
-//        mongo.setBindIp(MONGO_DB_URL);
-//        MongoClient mongoClient = mongo.getObject();
-//        MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, MONGO_DB_NAME);
-//        return mongoTemplate;
+//package org.camunda.assignment.configuration;
+//
+//import com.mongodb.client.MongoClient;
+//import de.flapdoodle.embed.mongo.MongodExecutable;
+//import de.flapdoodle.embed.mongo.MongodProcess;
+//import de.flapdoodle.embed.mongo.MongodStarter;
+//import de.flapdoodle.embed.mongo.config.MongodConfig;
+//import de.flapdoodle.embed.mongo.distribution.Version;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+//import org.springframework.data.mongodb.core.MongoTemplate;
+//
+//
+//import java.io.IOException;
+//
+//import de.flapdoodle.embed.mongo.Command;
+//import de.flapdoodle.embed.mongo.config.ImmutableMongodConfig;
+//
+//import de.flapdoodle.embed.mongo.distribution.Version;
+//import de.flapdoodle.embed.process.config.io.ProcessOutput;
+//import de.flapdoodle.embed.process.runtime.Network;
+//@Configuration
+//public class EmbeddedMongoConfig extends AbstractMongoClientConfiguration {
+//    /**
+//     * Return the name of the database to connect to.
+//     *
+//     * @return must not be {@literal null}.
+//     */
+//    @Configuration
+//    public class EmbeddedMongoConfiguration extends AbstractMongoClientConfiguration {
+//
+//        @Override
+//        protected String getDatabaseName() {
+//            return "test"; // Your preferred database name
+//        }
+//
+//        @Override
+//        @Bean(destroyMethod = "stop")
+//        public com.mongodb.client.MongoClient mongoClient() throws Exception {
+//            IMongodConfig mongodConfig = new MongodConfigBuilder()
+//                    .version(Version.Main.PRODUCTION)
+//                    .net(new de.flapdoodle.embed.mongo.config.Net("localhost", 27017, Network.localhostIsIPv6()))
+//                    .cmdOptions(Command.MongoD)
+//                    .build();
+//
+//            return new de.flapdoodle.embed.mongo.MongodExecutable(mongodConfig, new de.flapdoodle.embed.process.runtime.Starter(),
+//                    new de.flapdoodle.embed.process.store.ExtractedArtifactStoreBuilder()
+//                            .defaults(Command.MongoD)
+//                            .download(new de.flapdoodle.embed.process.extract.UserTempHome())
+//                            .build())
+//                    .start()
+//                    .getMongoClient(new ConnectionString("mongodb://localhost:" + mongodConfig.net().getPort()));
+//        }
 //    }
-
-
-}
+//
+//}
